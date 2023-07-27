5af5d1e5cdbdaa53e10d3511e8bb548bce98d624
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-07-27 13:30:34 +0200

    Change the api to movie one

--- spotifygame/index.js
+++ spotifygame/index.js
@@ -7,44 +7,56 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors());
-
-app.get("/auth", (req, res) => {
-  const id = process.env.SPOTIFY_ID;
-  const secret = process.env.CLIENT_SECRET;
+app.get("/topMovies", (req, res) => {
+  const apiKey = process.env.API_KEY;
+  const url = "https://api.themoviedb.org/3/movie/popular";
   const options = {
-    method: "POST",
-    params: {
-      grant_type: "client_credentials",
-      client_id: id,
-      client_secret: secret,
-    },
     headers: {
-      "Content-type": "application/x-www-form-urlencoded",
+      accept: "application/json",
+      Authorization:
+        `Bearer ${apiKey}`,
     },
   };
-  axios
-    .request("https://accounts.spotify.com/api/token", options)
-    .then((response) => res.json(response.data.access_token))
-    .catch((error) => {
-      console.error(error);
-    });
+  axios.get(url, options).then((response) => res.json(response.data.results)).catch((error) => console.log(error));
 });
+ 
+// app.get("/auth", (req, res) => {
+//   const id = process.env.SPOTIFY_ID;
+//   const secret = process.env.CLIENT_SECRET; 
+//   const options = { 
+//     method: "POST",
+//     params: {
+//       grant_type: "client_credentials",
+//       client_id: id,
+//       client_secret: secret, 
+//     },
+//     headers: {
+//       "Content-type": "application/x-www-form-urlencoded",
+//     },
+//   };
+//   axios
+//     .request("https://accounts.spotify.com/api/token", options)
+//     .then((response) => res.json(response.data.access_token))
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// });
 
-app.get("/search", (req, res) => {
-  const token = req.query.token;
-  const artist = req.query.artist;
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  axios.get(
-    `https://api.spotify.com/v1/search?q=${artist}&type=artist`,
-    options
-  ).then((response) => res.json(response.data.artists.items))
-});
+// app.get("/search", (req, res) => {
+//   const token = req.query.token;
+//   const artist = req.query.artist;
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token}`,
+//     },
+//   };
+//   axios.get(
+//     `https://api.spotify.com/v1/search?q=${artist}&type=artist`,
+//     options
+//   ).then((response) => res.json(response.data.artists.items))
+// });
 
 app.listen(8000, () => {
   console.log("Server is running");
--- spotifygame/src/App.js
+++ spotifygame/src/App.js
@@ -1,22 +1,19 @@
 import { useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import ArtistSearch from "./component/ArtistSearch";
 import { useStore } from "./stores/store";
 
 function App() {
-  const { setToken } = useStore();
   useEffect(() => {
     const fetchToken = () => {
-      axios.get("http://localhost:8000/auth").then(res => setToken(res.data));
+      axios.get("http://localhost:8000/topMovies").then(res => console.log(res.data));
     };
     fetchToken();
-  },[setToken])
+  },[])
   
   return (
-    <div className="App text-3xl bg-slate-400 h-min-full w-min-full">
-      <div className="mt-3 flex gap-3 justify-center">
-        <ArtistSearch/>
+    <div className="App text-3xl bg-[#121212] min-h-screen flex justify-center">
+      <div className="flex gap-3 justify-center w-[70%]">
       </div>
     </div>
   );
