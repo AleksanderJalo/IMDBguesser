002c35b01db08d40940dc189f6b45f582d44e015
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-07-27 18:13:38 +0200

    Work on Home screen

--- spotifygame/src/Pages/HomePage.js
+++ spotifygame/src/Pages/HomePage.js
@@ -4,18 +4,47 @@ import CategoryModal from "../component/CategoryModal";
 
 import { useStore } from "../stores/store";
 const HomePage = () => {
-    const { movies } = useStore();
+  const { movies } = useStore();
   return (
-    <div className="min-h-screen w-full flex">
-      <div className="flex flex-col justify-center items-center h-full w-1/2 bg-[#121212]">
+    <div className="min-h-screen w-full flex bg-[#53db90]">
+      <div className="flex flex-col justify-center items-center h-full w-1/2 ">
         <img src={ImdbLogo} alt="imdb logo" />
-        <div className="text-white text-8xl mt-2">GUESSER</div>
+        <div className="text-[#EDF5E1] text-8xl mt-2">GUESSER</div>
       </div>
-      {movies.length > 22 && <div className="w-1/2 h-full bg-[#212121] flex flex-col justify-center items-center gap-6">
-              <CategoryModal movieImg={`https://image.tmdb.org/t/p/original/${movies[3].backdrop_path}`}> Most Popular Movies</CategoryModal>
-              <CategoryModal movieImg={`https://image.tmdb.org/t/p/original/${movies[20].backdrop_path}`}> Top Rated Movies</CategoryModal>
-              <CategoryModal movieImg={`https://image.tmdb.org/t/p/original/${movies[22].backdrop_path}`}> Choose a Category</CategoryModal>
-      </div>}
+      {movies.length > 22 && (
+        <div className="w-1/2 h-full flex flex-col justify-center items-center gap-16 bg-[#05386B] font-roboto">
+          {/* <div className="text-4xl text-white text-left">Choose game mode:</div> */}
+          <div className="flex gap-6 justify-center ">
+            <CategoryModal
+              movieImg={`https://image.tmdb.org/t/p/original/${movies[3].backdrop_path}`}
+            >
+              {" "}
+              MOST POPULAR MOVIES
+            </CategoryModal>
+            <CategoryModal
+              movieImg={`https://image.tmdb.org/t/p/original/${movies[20].backdrop_path}`}
+            >
+              {" "}
+              TOP RATED MOVIES
+            </CategoryModal>
+          </div>
+          <div className="flex gap-6 justify-center">
+            {" "}
+            <CategoryModal
+              movieImg={`https://image.tmdb.org/t/p/original/${movies[22].backdrop_path}`}
+            >
+              {" "}
+              CHOOSE A CATEGORY 
+            </CategoryModal>
+            <CategoryModal
+              movieImg={`https://image.tmdb.org/t/p/original/${movies[24].backdrop_path}`}
+            >
+              {" "}
+              MOST POPULAR TV SHOWS
+            </CategoryModal>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- spotifygame/src/component/CategoryModal.js
+++ spotifygame/src/component/CategoryModal.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const CategoryModal = (props) => {
   return (
-    <div className="w-2/5 bg-[#121212]">
-      <img className="p-3" src={props.movieImg} />
-      <div className="bg-[#121212] text-3xl pb-3 text-white">
-        {props.children}
+    <div className="w-2/5 ">
+      <div className=" bg-[#379683] hover:bg-[#53db90] border-black border-8">
+        <img className="p-3" src={props.movieImg} />
+        <div className="text-3xl pb-3 text-[#05386B]  ">{props.children}</div>
       </div>
     </div>
   );
-}; 
+};
 
 export default CategoryModal;
