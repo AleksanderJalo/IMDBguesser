297e383d219d9dec0150e81d1d2ca42f8ef42826
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-07-31 17:09:30 +0200

    Gameplay Looks Good

--- spotifygame/src/Pages/GamePage.js
+++ spotifygame/src/Pages/GamePage.js
@@ -6,19 +6,22 @@ const GamePage = () => {
   const { movies } = useStore();
   const [firstMovie, setFirstMovie] = useState();
   const [secondMovie, setSecondMovie] = useState();
+  const [score, setScore] = useState(0);
   useEffect(() => {
     setFirstMovie(movies[Math.floor(Math.random() * movies.length)]);
     setSecondMovie(movies[Math.floor(Math.random() * movies.length)]);
   }, []);
-  const click = () => {
-    console.log(firstMovie);
-  };
+  const click = () => {};
   return (
     <div
       onClick={click}
-      className="flex flex-col h-min-screen  w-full bg-black text-white  items-center"
+      className="flex flex-col h-min-screen  w-full bg-black text-white  items-center relative gap-1"
     >
-      <div className="h-1/2">
+      <div className="absolute top-3 left-3 bg-white px-2 py-1 text-black z-10 border-2 border-black">{`Score: ${score}`}</div>
+      <div className="absolute bg-white p-3 text-4xl border-black border-4 rounded-full left-[50%] top-[50%] tranform translate-x-[-50%] translate-y-[-50%] z-30 text-black font-roboto flex justify-center items-center">
+        OR
+      </div>
+      <div className=" h-1/2 w-full p-1 border-white border-4">
         {firstMovie && (
           <MovieModal
             title={firstMovie.title}
@@ -26,7 +29,7 @@ const GamePage = () => {
           />
         )}
       </div>
-      <div className="h-1/2">
+      <div className="h-1/2 w-full  border-white border-4 p-1">
         {secondMovie && (
           <MovieModal
             title={secondMovie.title}
--- spotifygame/src/component/MovieModal.js
+++ spotifygame/src/component/MovieModal.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const MovieModal = (props) => {
   return (
-      <div className="h-full">
-          <img className='scale-[1] h-full' src={`https://image.tmdb.org/t/p/original${props.poster}` } />{props.title}</div>
-  )
-}
+    <div
+      className="h-full w-full bg-cover bg-center relative flex justify-center items-center "
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${props.poster})`,
+      }}
+    >
+      <div className="text-black text-xl font-roboto bg-white absolute p-3 w-4/5 shadow-black shadow-[8px_8px_0_0]">{props.title}</div>
+    </div> 
+  );
+};
 
-export default MovieModal  
+export default MovieModal;
