b3ff3ed9a9b23bd63f7cdc23f29a2c6b08900466
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-07-31 18:03:45 +0200

    More gameplay work

--- spotifygame/src/Pages/GamePage.js
+++ spotifygame/src/Pages/GamePage.js
@@ -9,11 +9,20 @@ const GamePage = () => {
   const [firstMovieScore, setFirstMovieScore] = useState();
   const [secondMovieScore, setSecondMovieScore] = useState();
   const [score, setScore] = useState(0);
+  const [didBothSpin, setDidBothSpin] = useState(false);
   const [movieClicked, setMovieClicked] = useState(false);
   useEffect(() => {
     setFirstMovie(movies[Math.floor(Math.random() * movies.length)]);
     setSecondMovie(movies[Math.floor(Math.random() * movies.length)]);
   }, []);
+  const afterShow = (isFirst) => {
+    if (isFirst) {
+      setSecondMovieScore(secondMovie.vote_average);
+    } else {
+      setFirstMovieScore(firstMovie.vote_average);
+    }
+    setDidBothSpin(true)
+  };
   const guess = (isFirst) => {
     if (!movieClicked) {
       if (isFirst) {
@@ -24,14 +33,28 @@ const GamePage = () => {
     }
     setMovieClicked(true);
   };
-
+  const check = () => { 
+    console.log("check")
+  };
   return (
     <div className="flex flex-col h-min-screen  w-full bg-black text-white  items-center relative gap-1">
       {firstMovieScore && (
-        <ShowScore score={firstMovieScore} isFirstMovie={true} />
+        <ShowScore
+          score={firstMovieScore}
+          isFirstMovie={true}
+          afterShow={afterShow}
+          didBothSpin={didBothSpin}
+          check = {check}
+        />
       )}
       {secondMovieScore && (
-        <ShowScore score={secondMovieScore} isFirstMovie={false} />
+        <ShowScore
+          score={secondMovieScore}
+          isFirstMovie={false}
+          afterShow={afterShow}
+          didBothSpin={didBothSpin}
+          check = {check}
+        />
       )}
       <div className="absolute top-3 left-3 bg-white px-2 py-1 text-black z-10 border-2 border-black">{`Score: ${score}`}</div>
       <div className="absolute bg-white p-3 text-4xl border-black border-4 rounded-full left-[50%] top-[50%] tranform translate-x-[-50%] translate-y-[-50%] z-30 text-black font-roboto flex justify-center items-center">
--- spotifygame/src/component/ShowScore.js
+++ spotifygame/src/component/ShowScore.js
@@ -2,16 +2,31 @@ import React, { useEffect, useState } from "react";
 
 const ShowScore = (props) => {
   const [score, setScore] = useState(0);
-    useEffect(() => {
-        let interval;
-        if (score === 0) {
+    const [checked, setChecked] = useState(false);
+    const [finish, setFinish] = useState(false);
+  useEffect(() => {
+    let interval;
+    if (score === 0) {
       const interval = setInterval(() => {
-        setScore((prev) => Math.round((prev + 0.1)*10)/10);
+        setScore((prev) => Math.round((prev + 0.1) * 10) / 10);
       }, 10);
     }
 
-        if (score > props.score) {
-            setScore(props.score)
+    if (score > props.score) {
+      setScore(props.score);
+      if (!props.didBothSpin) {
+        setTimeout(() => {
+          props.afterShow(props.isFirstMovie);
+        }, 400);
+      } else {
+        setChecked(true);
+      }
+      if (checked && !finish) {
+        setFinish(true);
+        setTimeout(() => {
+          props.check();
+        }, 500);
+      }
       clearInterval(interval);
     }
   });
